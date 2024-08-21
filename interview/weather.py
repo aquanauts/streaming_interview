abd4b64fe32b2769006fe8cc6fe7fb0216a2e1c0
laurenkiachian <agirlnamedlauren@gmail.com>
2024-08-21 10:29:29 -0700

    Lauren Kiachian commit

--- interview/weather.py
+++ interview/weather.py
@@ -2,5 +2,62 @@ from typing import Any, Iterable, Generator
 
 
 def process_events(events: Iterable[dict[str, Any]]) -> Generator[dict[str, Any], None, None]:
+    weather_stations = {}
+    recent_timestamp = None
+
     for line in events:
-        yield line
+        if line["type"] == "sample":
+            station = line["stationName"]
+            timestamp = line["timestamp"]
+            temp = line["temperature"]
+
+            if recent_timestamp is None or timestamp > recent_timestamp:
+                recent_timestamp = timestamp
+
+            if station not in weather_stations:
+                weather_stations[station] = {
+                    "high": temp,
+                    "low": temp,
+                    "timestamp": timestamp
+                }
+            else:
+                if temp < weather_stations[station]["low"]:
+                    weather_stations[station]["low"] = temp
+                if temp > weather_stations[station]["high"]:
+                    weather_stations[station]["high"] = temp
+                weather_stations[station]["timestamp"] = timestamp
+
+        elif line["type"] == "control":
+            if line["command"] == "snapshot":
+                if recent_timestamp is not None:
+                    snapshot = {
+                        "type": "snapshot",
+                        "asOf": recent_timestamp,
+                        "stations": {
+                            station: {
+                                "high": data["high"],
+                                "low": data["low"]
+                            } for station, data in weather_stations.items()
+                        }
+                    }
+                    yield snapshot
+
+            elif line["command"] == "reset":
+                if recent_timestamp is not None:
+                    reset = {
+                        "type": "reset",
+                        "asOf": recent_timestamp,
+                    }
+                    yield reset
+
+                weather_stations.clear()
+                recent_timestamp = None
+
+            else:
+                raise ValueError(
+                    f"Unknown control command: {line['command']}. \n"
+                    "Please verify input.")
+        else:
+            raise ValueError(f"Unknown message type: {line['type']}. \n"
+                             "Please verify input.")
+        
--- interview/weather_test.py
+++ interview/weather_test.py
@@ -1,4 +1,146 @@
 from . import weather
 
-def test_replace_me():
-    assert [{}] == list(weather.process_events([{}]))
+# def test_no_events():
+#     assert [{}] == list(weather.process_events([{}]))
+
+def test_sample_events():
+
+    # Test 1: single weather event
+    events = [
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531000000, "temperature": 55.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "snapshot",
+            "asOf": 1672531000000,
+            "stations": {
+                "Ohio Street": {"high": 55.0, "low": 55.0}
+            }
+        }
+    ], f"Test 1 failed with output: {output}"
+
+    # Test 2: multiple weather events, one station
+    events = [
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531000000, "temperature": 15.0},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672535000000, "temperature": 20.0},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672539000000, "temperature": 10.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "snapshot",
+            "asOf": 1672539000000,
+            "stations": {
+                "Ohio Street": {"high": 20.0, "low": 10.0}
+            }
+        }
+    ], f"Test 2 failed with output: {output}"
+
+    # Test 3: multiple weather events, multiple stations
+    events = [
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531000000, "temperature": 15.0},
+        {"type": "sample", "stationName": "Calumet", \
+         "timestamp": 1672535000000, "temperature": 5.0},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672539000000, "temperature": 25.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "snapshot",
+            "asOf": 1672539000000,
+            "stations": {
+                "Ohio Street": {"high": 25.0, "low": 15.0},
+                "Calumet": {"high": 5.0, "low": 5.0}
+            }
+        }
+    ], f"Test 3 failed with output: {output}"
+
+    # Test 4: reset
+    events = [
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531000000, "temperature": 15.0},
+        {"type": "control", "command": "reset"},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672539000000, "temperature": 25.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "reset",
+            "asOf": 1672531000000
+        },
+        {
+            "type": "snapshot",
+            "asOf": 1672539000000,
+            "stations": {
+                "Ohio Street": {"high": 25.0, "low": 25.0}
+            }
+        }
+    ], f"Test 4 failed with output: {output}"
+
+    # Test 5: snapshot with no data
+    events = [
+        {"type": "control", "command": "snapshot"},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531000000, "temperature": 15.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "snapshot",
+            "asOf": 1672531000000,
+            "stations": {
+                "Ohio Street": {"high": 15.0, "low": 15.0}
+            }
+        }
+    ], f"Test 4 failed with output: {output}"
+
+    # Test 6: reset with no data
+    events = [
+        {"type": "control", "command": "reset"},
+        {"type": "sample", "stationName": "Ohio Street", \
+         "timestamp": 1672531200000, "temperature": 15.0},
+        {"type": "control", "command": "snapshot"}
+    ]
+    output = list(weather.process_events(events))
+    assert output == [
+        {
+            "type": "snapshot",
+            "asOf": 1672531200000,
+            "stations": {
+                "Ohio Street": {"high": 15.0, "low": 15.0}
+            }
+        }
+    ], f"Test 4 failed with output: {output}"
+
+    # Test 7: invalid input
+    events = [
+        {"type": "unknown", "stationName": "Ohio Street", \
+         "timestamp": 1672531200000, "temperature": 15.0},
+    ]
+    try:
+        output = list(weather.process_events(events))
+        assert False, "Test 7 failed: Exception not raised."
+    except ValueError as e:
+        assert "Please verify input." in str(e), f"Test 7 failed: {str(e)}"
+
+    # Test 8: unknown command
+    events = [{"type": "control", "command": "unknown"}]
+    try:
+        output = list(weather.process_events(events))
+        assert False, "Test 8 failed: Exception not raised."
+    except ValueError as e:
+        assert "Please verify input." in str(e), f"Test 8 failed: {str(e)}"
+        
